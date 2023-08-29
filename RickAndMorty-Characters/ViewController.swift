710f661edbb95bc4adb522dc39da794747bb27db
Alejandro Rguez Cañete <ale.rodca04@gmail.com>
2023-08-29 10:46:05 +0200

    Initial Commit

---
+++ RickAndMorty-Characters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty-Characters
+//
+//  Created by Alejandro Rodríguez Cañete on 29/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
